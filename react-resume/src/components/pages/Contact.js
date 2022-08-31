7b0863b3fcae0108836ace07be282c52134cb559
justnoserena <justnoserena@gmail.com>
2022-08-30 21:06:23 -0700

    Added code and readme

---
+++ react-resume/src/components/pages/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact Page</h1>
+      <p>
+        Phone: (480)-326-4530
+        Gmail: Justnoserena
+        Github Username: Justnoserena
+      </p>
+    </div>
+  );
+}
